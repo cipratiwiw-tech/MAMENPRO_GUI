7ac7ea2a5381c8f8f1cd78439c42d6965afd98b8
lailafau02-stack <lailafau02@gmail.com>
2026-01-17 18:29:50 +0700

    rombak semuanya, hapus file gak perlu

--- engine/interactive_text.py
+++
@@ -1,9 +0,0 @@
-from engine.interaction.interactive import InteractiveLayerMixin
-from effects.text.text_advanced import TextAdvancedEffect
-
-class InteractiveTextLayer(InteractiveLayerMixin, TextAdvancedEffect):
-    """
-    Wrapper GUI untuk TextAdvancedEffect
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
